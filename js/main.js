4992405a4f995fb2ba1f9629650a25e01c96cb46
Rammah Mohamed <Rammahmohamed.rm@gmail.com>
2023-03-07 09:45:29 +0200

    Uploaded

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Start Shuffle
+// window.onresize = () => {
+//     if (window.innerWidth >= 992) {
+//         document.querySelectorAll(".nav-item").forEach(i => {
+//             i.addEventListener("click", e => {
+//                 document.querySelectorAll(".nav-item").forEach(li => {
+//                     li.classList.remove("active");
+//                     li.removeAttribute("aria-current");
+//                 });
+//                 e.target.classList.add("active");
+//                 document.querySelectorAll(".feature-work .all").forEach(el => {
+//                     el.style.opacity = "0.2";
+//                 })
+//                 document.querySelectorAll(e.target.dataset.img).forEach(img => {
+//                     img.style.opacity = "1";
+//                 })
+//             })
+//         })
+//     }
+// }
+//End Shuffle
